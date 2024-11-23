a3a44e8f8a2c65a99a9efc3b1f4067c32f1a0c8c
kirbythomson <kirby.thomson@riotinto.com>
2024-11-23 10:30:55 +1000

    Added nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ import HomePage from "./pages/HomePage.jsx";
+ import ProjectPage from "./pages/ProjectPage.jsx";
+ 
+ import NavBar from "./components/NavBar.jsx";
+ 
+ const router = createBrowserRouter([
+   {
+       path: "/",
+       element: <NavBar />,
+       children: [
+           { path: "/", element: <HomePage /> },
+           { path: "/project", element: <ProjectPage /> },
+       ],
+   },
+ ]);
+
+ ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      {/* Here we wrap our app in the router provider so they render */}
+      <RouterProvider router={router} />
+   </React.StrictMode>
+ );
