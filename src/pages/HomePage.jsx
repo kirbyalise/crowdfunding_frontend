9daa874e71d45f8e267581de0ad2b0a2024d093f
kirbythomson <kirby.thomson@riotinto.com>
2024-11-23 12:30:16 +1000

    Finsihed exercises

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,12 @@
-function HomePage() {
-    return <h1>This is the home page.</h1>;
-  }
-  
-  export default HomePage;
+import { allProjects } from "../data";
+import ProjectCard from "../components/ProjectCard";
+
+function HomePage() {return (
+    <div>
+        {allProjects.map((projectData, key) => {
+            return <ProjectCard key={key} projectData={projectData} />;
+        })}
+    </div>
+    );
+}
+export default HomePage;
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,5 +1,21 @@
 function ProjectPage() {
-    return <h1>This is the project page.</h1>;
-  }
-  
-  export default ProjectPage
+    return (
+        <div>
+            <h2>{oneProject.title}</h2>
+            <h3>Created at: {oneProject.date_created}</h3>
+            <h3>{`Status: ${oneProject.is_open}`}</h3>
+            <h3>Pledges:</h3>
+            <ul>
+                {oneProject.pledges.map((pledgeData, key) => {
+                    return (
+                        <li key={key}>
+                            {pledgeData.amount} from {pledgeData.supporter}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+    export default ProjectPage
