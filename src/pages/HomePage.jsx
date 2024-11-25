25eab483ad14d5b6b940cde97c49936fab14e9d8
kirbythomson <kirby.thomson@riotinto.com>
2024-11-25 17:17:56 +1000

    Finished tutorial steps

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard";
+import "../HomePage.css";
 
 function HomePage() {return (
-    <div>
+    <div id="project-list">
         {allProjects.map((projectData, key) => {
             return <ProjectCard key={key} projectData={projectData} />;
         })}
